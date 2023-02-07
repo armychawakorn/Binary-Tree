f334d0490e8d9d716351a06407b8ad3733423865
RosiRosinante <armychawakorn@gmail.com>
2023-02-07 18:05:44 +0700

    first commit

---
+++ Binary Search Tree.py
@@ -0,0 +1,35 @@
+class myNode:
+  def __init__(self,data) :
+    self.Left = None
+    self.Right = None
+    self.data = data
+  def insert(self,data):       
+    if self.data:
+      if data < self.data:
+        if self.Left is None:
+          self.Left = myNode(data)
+        else:
+          self.Left.insert(data)
+      else:
+        if self.Right is None:
+          self.Right = myNode(data)
+        else:
+          self.Right.insert(data)
+    return self
+  def PrintTree(self):
+    if self.Left:
+      self.Left.PrintTree()
+      print(self.data),
+    if self.Right:
+      self.Right.PrintTree()
+  def inorderTraversal(self, root):
+    res = []
+    if root:
+      res = self.inorderTraversal(root.Left)
+      res.append(root.data)
+      res = res + self.inorderTraversal(root.Right)
+    return res
+
+root = myNode(2566).insert(2).insert(5).insert(6).insert(6).insert(25).insert(66).insert(20).insert(23).insert(2)
+
+print(root.inorderTraversal(root= root))
---
+++ BinaryTree.py
@@ -0,0 +1,23 @@
+class Node:
+  def __init__(self,myData):
+    self.Left = None
+    self.Right = None
+    self.Data = myData
+    
+root = Node("P")
+
+root.Left = Node("O")
+root.Left.Left = Node("L")
+root.Left.Right = Node("Y")
+root.Left.Left.Left = Node("N")
+root.Left.Left.Right = Node("T")
+root.Left.Right.Left = Node("S")
+root.Left.Right.Right = Node("A")
+
+root.Right = Node("L")
+root.Right.Left = Node("W")
+root.Right.Right = Node("A")
+root.Right.Left.Left = Node("C")
+root.Right.Left.Right = Node("R")
+root.Right.Right.Left = Node("E")
+root.Right.Right.Right = Node("C")
